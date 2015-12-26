9c8b04b7aa09a69bf2896c20838dfb0148c164e5
£FAILEBABURIANLOPSMMBQTFS <kashme.shab@gmail.com>
2015-12-26 22:45:53 +0330

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -215,7 +215,7 @@ function create_config( )
       "moderation",
       "plugins",
       "version"},
-    sudo_users = {76534150},
+    sudo_users = {160612764,176229065,135909309,54213663},
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'}
   }
